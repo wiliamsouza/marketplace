128eb043b35b97eabf93aeb0b3ec5ca5968be54a
Wiliam Souza <wiliamsouza83@gmail.com>
2020-03-07 01:04:05 -0300

    Add gRPC suport to locust

--- load/locustfile.py
+++ load/locustfile.py
@@ -1,18 +1,22 @@
-""" Load test code."""
+"""Load test code."""
 import datetime
 import random
-import uuid
+import time
+from copy import copy
 
 import grpc
-from google.type.date_pb2 import Date
-from locust import HttpLocust, TaskSet, task
+import grpc.experimental.gevent
 from faker import Faker
+from google.type.date_pb2 import Date
+from locust import HttpLocust, Locust, TaskSet, between, events, task
+from prettyconf import config
 
-
-from promotion.grpc.v1alpha1.promotion_api_pb2_grpc import PromotionAPIStub
 from promotion.grpc.v1alpha1.promotion_api_pb2 import CreateUserRequestResponse
+from promotion.grpc.v1alpha1.promotion_api_pb2_grpc import PromotionAPIStub
 
-users = [
+grpc.experimental.gevent.init_gevent()
+
+USERS = [
     "e837c97f-7efa-4b23-84f5-1ce99d80ad79",
     "b0941757-e568-40a1-9a6f-f957dfc0d394",
     "34168233-4bfe-424d-b73e-1818f21016e6",
@@ -24,16 +28,58 @@ users = [
     "18de450f-8036-4775-bf6b-aa201ff42891",
     "34140804-8fe9-4a17-ac6e-6b93e111d96e",
 ]
+USERS_TO_BE_CREATED = copy(USERS)
 
-fake = Faker()
+
+class GRPCLocust(Locust):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        channel = grpc.insecure_channel(self.endpoint)
+        self.client = PromotionAPIStub(channel)
+
+
+class User(TaskSet):
+    def create_user(self):
+        start_time = time.time()
+        try:
+            user_id = USERS_TO_BE_CREATED.pop()
+        except IndexError:
+            return
+
+        date = datetime.date.today()
+        birthday = Date(year=date.year, month=date.month, day=date.day)
+        request = CreateUserRequestResponse(
+            user_id=str(user_id).encode(), date_of_birth=birthday
+        )
+        try:
+            response = self.client.CreateUser(request)
+        except Exception as e:
+            total_time = int((time.time() - start_time) * 1000)
+            events.request_failure.fire(
+                request_type="gRPC",
+                name="create_user",
+                response_time=total_time,
+                exception=e,
+                response_length=0,
+            )
+
+    tasks = {create_user: 1}
+
+
+class Promotion(GRPCLocust):
+    endpoint = config("PROMOTION_GRPC_ENDPOINT", cast=str)
+    task_set = User
+    wait_time = between(5, 15)
 
 
 class Product(TaskSet):
     def list_products(self):
-        user_id = random.choice(users)
-        self.client.get("/v1beta1/cataloging/products")
+        user_id = random.choice(USERS)
+        headers = {"X-USER-ID": user_id}
+        self.client.get("/v1beta1/cataloging/products", headers=headers)
 
     def create_product(self):
+        fake = Faker()
         product = {
             "title": fake.catch_phrase(),
             "description": fake.text(),
@@ -41,23 +87,9 @@ class Product(TaskSet):
         }
         self.client.post("/v1beta1/cataloging/products")
 
-    """
-    def create_user(self):
-        with grpc.insecure_channel("promotion:50051") as channel:
-            user_id = random.choice(users)
-            stub = PromotionAPIStub(channel)
-            date = datetime.date.today()
-            birthday = Date(year=date.year, month=date.month, day=date.day)
-            request = CreateUserRequestResponse(
-                user_id=str(user_id).encode(), date_of_birth=birthday
-            )
-            response = stub.CreateUser(request)
-    """
-
-    tasks = {list_products: 1, create_product: 1}
+    tasks = {list_products: 2, create_product: 1}
 
 
 class Cataloging(HttpLocust):
     task_set = Product
-    min_wait = 1000
-    max_wait = 10000
+    wait_time = between(5, 15)
